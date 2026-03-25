7d4804dab70de472fa6af63961efb571d62dc3a4
codewithseth <codewithseth2026@gmail.com>
2026-03-25 15:57:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# meatika-api
---
+++ src/test/java/com/codewithseth/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithseth.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
