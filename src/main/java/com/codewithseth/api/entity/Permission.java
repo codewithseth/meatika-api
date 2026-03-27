ec828f1f137f82ca3b7bd6ebda5016a513814597
codewithseth <codewithseth2026@gmail.com>
2026-03-27 12:10:30 +0700

    Add Permission entity for access control management

---
+++ src/main/java/com/codewithseth/api/entity/Permission.java
@@ -0,0 +1,40 @@
+package com.codewithseth.api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "permissions")
+public class Permission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String uuid;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String alias;
+
+    @Column(name = "name_en", nullable = false, length = 50)
+    private String nameEn;
+
+    @Column(name = "name_km", nullable = false, length = 50)
+    private String nameKm;
+
+    @Column(name = "group_name_en", nullable = false, length = 50)
+    private String groupNameEn;
+
+    @Column(name = "group_name_km", nullable = false, length = 50)
+    private String groupNameKm;
+
+}
