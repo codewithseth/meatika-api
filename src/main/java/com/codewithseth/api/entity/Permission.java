4f901ab53bbc132cf1d3330ce4d3b5a4a3d4768d
codewithseth <codewithseth2026@gmail.com>
2026-03-27 12:34:06 +0700

    Add ManyToMany relationship between Role and Permission entities

--- src/main/java/com/codewithseth/api/entity/Permission.java
+++ src/main/java/com/codewithseth/api/entity/Permission.java
@@ -1,10 +1,14 @@
 package com.codewithseth.api.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -36,5 +40,8 @@ public class Permission extends BaseEntity {
 
     @Column(name = "group_name_km", nullable = false, length = 50)
     private String groupNameKm;
+
+    @ManyToMany(mappedBy = "permissions")
+    private Set<Role> roles = new LinkedHashSet<Role>();
 
 }
--- src/main/java/com/codewithseth/api/entity/Role.java
+++ src/main/java/com/codewithseth/api/entity/Role.java
@@ -1,10 +1,17 @@
 package com.codewithseth.api.entity;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -36,5 +43,13 @@ public class Role extends BaseEntity {
 
     @Column(name = "description_km", columnDefinition = "TEXT")
     private String descriptionKm;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "role_permissions",
+        joinColumns = @JoinColumn(name = "role_id"),
+        inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions = new LinkedHashSet<Permission>();
 
 }
