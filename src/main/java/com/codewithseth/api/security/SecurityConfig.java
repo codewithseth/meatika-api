376621ddf75a0375d841e71f7e62e4f28e1005d1
codewithseth <codewithseth2026@gmail.com>
2026-03-28 22:25:32 +0700

    Add JWTTokenValidatorFilter for JWT authentication and update SecurityConfig to include the filter

--- src/main/java/com/codewithseth/api/security/SecurityConfig.java
+++ src/main/java/com/codewithseth/api/security/SecurityConfig.java
@@ -13,19 +13,28 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.codewithseth.api.filter.JWTTokenValidatorFilter;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
-    
+
+    private final JWTTokenValidatorFilter jwtTokenValidatorFilter;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(c -> c.disable())
             .httpBasic(c -> c.disable())
             .formLogin(c -> c.disable())
+            .addFilterBefore(jwtTokenValidatorFilter, BasicAuthenticationFilter.class)
             .authorizeHttpRequests((authorize) -> authorize
                 .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/api/v1/auth/**").permitAll()
                 .anyRequest().authenticated()
