e26f92fab6aae7c733df932c520b5439732c1347
codewithseth <codewithseth2026@gmail.com>
2026-03-29 13:08:00 +0700

    Enhance JWTTokenValidatorFilter to improve error handling and logging for JWT validation; add specific exceptions for better clarity in responses

--- src/main/java/com/codewithseth/api/filter/JWTTokenValidatorFilter.java
+++ src/main/java/com/codewithseth/api/filter/JWTTokenValidatorFilter.java
@@ -3,15 +3,18 @@ package com.codewithseth.api.filter;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -25,6 +28,7 @@ import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JWTTokenValidatorFilter extends OncePerRequestFilter {
@@ -55,14 +59,32 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
                     }
                 }
             } catch (ExpiredJwtException exception) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.getWriter().write("Token Expired");
+                log.error("JWT token has expired: {}", exception.getMessage());
+                sendErrorResponse(response, HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+                return;
+            } catch (UnsupportedJwtException exception) {
+                log.error("Unsupported JWT token: {}", exception.getMessage());
+                sendErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
+                return;
+            } catch (MalformedJwtException exception) {
+                log.error("Malformed JWT token: {}", exception.getMessage()); // tested
+                sendErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
+                return;
+            } catch (SignatureException exception) {
+                log.error("Invalid JWT signature: {}", exception.getMessage()); //tested
+                sendErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
+                return;
+
+            } catch (IllegalArgumentException exception) {
+                log.error("JWT token is null or empty: {}", exception.getMessage());
+                sendErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
                 return;
             } catch (Exception exception) {
-                throw new BadCredentialsException("Invalid Token received!");
+                log.error("Error validating JWT token: {}", exception.getMessage());
+                sendErrorResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, exception.getMessage());
+                return;
             }
         }
-
         filterChain.doFilter(request, response);
     }
 
@@ -73,6 +95,20 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
         return path.startsWith("/api/v1/auth") ||
                path.startsWith("/v3/api-docs") ||
                path.startsWith("/swagger-ui");
+    }
+
+    private static final String ERROR_MESSAGE = "An error occurred while validating the JWT token";
+    private void sendErrorResponse(HttpServletResponse response, int code, String data) throws IOException {
+        response.setStatus(code);
+        response.setContentType("application/json");
+        
+        String jsonResponse = String.format(
+            "{\"flag\": false, \"code\": %d, \"message\": \"%s\", \"data\": \"%s\"}", 
+            code, ERROR_MESSAGE, data
+        );
+
+        response.getWriter().write(jsonResponse);
+
     }
 
 }
