86e2da34dca96429b80ccfd714b6a7b314439e43
codewithseth <codewithseth2026@gmail.com>
2026-03-30 11:13:20 +0700

    Refactor AuditorAwareImpl to retrieve the current authenticated user's username; return "Anonymous user" for unauthenticated requests

--- src/main/java/com/codewithseth/api/config/AuditorAwareImpl.java
+++ src/main/java/com/codewithseth/api/config/AuditorAwareImpl.java
@@ -3,14 +3,34 @@ package com.codewithseth.api.config;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import com.codewithseth.api.entity.Account;
 
 @Component("auditorAwareImpl")
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("system");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
+            return Optional.of("Anonymous user");
+        }
+
+        Object principal = authentication.getPrincipal();
+        
+        String username;
+
+        if (principal instanceof Account account) {
+            username = account.getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        return Optional.of(username);
     }
 
 }
